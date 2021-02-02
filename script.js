dab6f801c1623bbb5e3de2e3e47488f6e9819de5
mbm_1607 <muhammadkhan1607@gmail.com>
2021-02-02 13:47:02 +0500

    Layout the Piano and add click event and blink
    Correct the Piano button layout. Add a blinking
    effect to buttons. Made clicking press the buttons
    as well.

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ document.addEventListener("keypress", (e) => {
     const key = e.key.toUpperCase();
     if ("ASDFGHJWETYU".includes(key)) {
         let audio = new Audio(`audio/${key}.mp3`);
-        audio.play();
+        audio.play().then(() => {
+            blink(document.getElementById(key))
+        });
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".piano-key").forEach((key) => {
+       key.addEventListener("click", () => {
+           blink(key)
+       })
+    });
+});
+
+function blink(key) {
+    key.classList.add("pressed");
+    setTimeout(() => key.classList.remove("pressed"), 500)
+}
